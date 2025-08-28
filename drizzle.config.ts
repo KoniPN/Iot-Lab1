bd9bcdee7d5f3a0d13dd82168b2cc9a6063d60f3
KoniPN <kong.18th@gmail.com>
2025-08-29 04:25:17 +0700

    -

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
+// drizzle.config.ts
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: "postgresql",
   schema: "./api/db/schema.ts",
-  dbCredentials: {
-    url: process.env.DATABASE_URL as string,
-  },
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: { url: process.env.POSTGRES_URL! },
   casing: "snake_case",
 });
